13a37f1c7f78f26d6c291e355cfd060853010b30
Luiz Gallas <luizgallas@outlook.com>
2022-04-18 20:39:04 -0300

    Ajustado para poder receber multiplos usuários

--- cellphone.js
+++ cellphone.js
@@ -1,7 +1,7 @@
 const net = require('net');
 var readline = require('readline');
 
-const timeoutLimit = 30;
+const timeoutLimit = 20;
 
 const client = new net.Socket();
 
@@ -10,11 +10,11 @@ var reader = readline.createInterface({
     output: process.stdout
 });
 
-const handleCallback = () => {
+const handlePNEApprouch = () => {
     client.write(JSON.stringify({ isPNE: true }));
     reader.question('O semáforo está fechando, aperte qualquer tecla quando houver movimentação do dispositivo', () => {
         console.log('Atravessando');
-        const mockTimeToCross = Math.round(Math.floor(Math.random() * 50));
+        const mockTimeToCross = Math.round(Math.floor(Math.random() * 30));
         const willTimeout = mockTimeToCross > timeoutLimit;
         const realAvailableTimeToCross = willTimeout ? timeoutLimit : mockTimeToCross;
 
@@ -25,9 +25,28 @@ const handleCallback = () => {
             } else {
                 console.log('Atravessei');
                 client.write(JSON.stringify({ shouldOpen: true })); 
+
+                client.destroy();
+                process.exit();
             }
         }, [realAvailableTimeToCross * 1000]);
     })
 }
 
+const handleCallback = () => {
+    reader.question('É PNE?: SIM = 1, NAO = 2', (answer) => {
+        const parsedAnswer = Number(answer);
+
+        if (parsedAnswer === 1) {
+            handlePNEApprouch();
+        } else if (parsedAnswer === 2) {
+            console.log('Pessoa não é portadora de necessidade especial, nenhuma ação necessária');
+
+            client.destroy();
+            process.exit();
+        }
+    });
+}
+
 client.connect(4000, '127.0.0.1', handleCallback); // Server
+// client.connect(4000, '172.30.133.245', handleCallback); // Server
--- trafficlight.js
+++ trafficlight.js
@@ -1,5 +1,6 @@
 const net = require('net');
 
+let isOpen = true;
 const server = net.createServer((socket) => {
     socket.on('data', (data) => {
         const parsedData = JSON.parse(data);
@@ -7,6 +8,8 @@ const server = net.createServer((socket) => {
         let intervalId = null;
         let counter = 10;
         const handleTrafficLightStatus = (isOpening) => {
+            isOpen = isOpening;
+
             if (counter >= 1) {
                 isOpening 
                     ? console.log(`Semáforo abrindo em ${counter} segundos`)  
@@ -14,6 +17,7 @@ const server = net.createServer((socket) => {
                 counter--;
             } else {
                 clearInterval(intervalId);
+
                 isOpening 
                     ? console.log('Semáforo aberto')
                     : console.log('Semáforo fechado');
@@ -23,9 +27,9 @@ const server = net.createServer((socket) => {
             }
         }
         
-        if (parsedData.isPNE) {
+        if (parsedData.isPNE && isOpen) {
             intervalId = setInterval(() => handleTrafficLightStatus(false), 1000);  
-        } else if (parsedData.shouldOpen) {
+        } else if (parsedData.shouldOpen && !isOpen) {
             intervalId = setInterval(() => handleTrafficLightStatus(true), 1000);  
     }});
 });
